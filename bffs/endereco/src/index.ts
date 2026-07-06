9793f4f09ccefaea86953ec6fff55cd2a8744183
Marco Mendes <corelio@gmail.com>
2026-07-05 21:39:43 -0300

    feat(bff-endereco): bootstrap do servidor e README do serviço

---
+++ bffs/endereco/src/index.ts
@@ -0,0 +1,9 @@
+import { createApp } from './app.ts'
+import { loadConfig } from './config.ts'
+
+const config = loadConfig()
+const app = createApp()
+
+app.listen(config.port, () => {
+  console.log(`BFF-endereco ouvindo em http://localhost:${config.port}`)
+})
